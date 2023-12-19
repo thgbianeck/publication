7e26793bcd86156066f882e08e85e6556d7c4ed5
Bianeck <thiagobianeck@gmail.com>
2023-12-19 13:41:21 -0300

    feat: adiciona classes para tratamento de exceções
    - Criada a classe 'ErrorResponse' para representar detalhes de erro.
    - Implementada classe 'ApiExceptionHandler' para lidar com exceções globais no projeto.
    - Adicionado método 'handlerMethodException' para tratar exceções do tipo 'Exception' e gerar resposta padronizada.

---
+++ src/main/java/br/com/bieniek/publication/domain/ErrorResponse.java
@@ -0,0 +1,18 @@
+package br.com.bieniek.publication.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+
+    private Long timestamp;
+    private Integer status;
+    private String message;
+    private String path;
+}
---
+++ src/main/java/br/com/bieniek/publication/handler/ApiExceptionHandler.java
@@ -0,0 +1,30 @@
+package br.com.bieniek.publication.handler;
+
+import br.com.bieniek.publication.domain.ErrorResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handlerMethodException(
+            final Exception exception, final HttpServletRequest request) {
+
+        var error = ErrorResponse.builder()
+                .timestamp(System.currentTimeMillis())
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message(exception.getMessage())
+                .path(request.getRequestURI())
+                .build();
+
+        log.error("[Error] payload = {}", error, exception);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+
+    }
+}
