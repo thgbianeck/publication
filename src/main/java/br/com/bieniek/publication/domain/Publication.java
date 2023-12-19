3573e795ebbf2e4731bdde986774900d11bf2427
Bianeck <thiagobianeck@gmail.com>
2023-12-19 01:27:50 -0300

    feat: adiciona classes no pacote de domínio
    Foram criadas as seguintes classes no pacote de domínio (package br.com.bieniek.publication.domain):
    
    1. Classe 'Publication':
        - Atributos: title, imageUrl, text e comments.
        - Métodos gerados automaticamente pelo Lombok: construtores padrão e completo, getters e setters.
    
    2. Classe 'Comment':
        - Atributos: author e text.
        - Métodos gerados automaticamente pelo Lombok: construtores padrão e completo, getters e setters.
    
    Essas classes fazem parte da estrutura do domínio do projeto e são utilizadas para representar publicações e comentários associados.

---
+++ src/main/java/br/com/bieniek/publication/domain/Publication.java
@@ -0,0 +1,18 @@
+package br.com.bieniek.publication.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Publication {
+
+    private String title;
+    private String imageUrl;
+    private String text;
+    private List<Comment> comments;
+}
