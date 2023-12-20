959cfac517d91dc3b3ca827fa124a71a9c41480d
Bianeck <thiagobianeck@gmail.com>
2023-12-20 00:18:47 -0300

    feat: ajuste para o json do response para não retornar nulo

--- src/main/java/br/com/bieniek/publication/controller/response/PublicationResponse.java
+++ src/main/java/br/com/bieniek/publication/controller/response/PublicationResponse.java
@@ -1,6 +1,7 @@
 package br.com.bieniek.publication.controller.response;
 
 import br.com.bieniek.publication.domain.Comment;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,6 +11,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class PublicationResponse {
 
     private String id;
